2fc699b8db692b63c28265ad39244d8dd242b053
JuanDGM <juan84_10@hotmail.com>
2019-01-07 12:22:00 -0500

    cuatro

--- sw.js
+++ sw.js
@@ -1,26 +1,18 @@
 
-
 const CACHE_STATIC = 'static-v1';
 
-
 const APP_SHELL=[
-    'index.php',
+    'index.html',
     'js/app.js',
     'sw.js',
     'manifest.json'
-    
 ];
 
 
-
 self.addEventListener('install', e=>{
-    
     const respuesta = caches.open(CACHE_STATIC).then(res=>{
-        
-        res.addAll(APP_SHELL);
-        
+       res.addAll(APP_SHELL);
     });
-    
     e.waitUntil(respuesta);
 });
 
